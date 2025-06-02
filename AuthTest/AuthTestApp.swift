d36212fff825942b8723b6292f3f2886bd55d56c
hirenmetaliya2 <hiren.thetatechnolabs.com>
2025-06-02 10:59:02 +0530

    Initial Commit

---
+++ AuthTest/AuthTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthTestApp.swift
+//  AuthTest
+//
+//  Created by Hiren Metaliya on 02/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AuthTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
